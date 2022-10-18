c23003da038f29c35638e8b25ab63de5fd8ed7bf
cooldebs <doduronbi83@gmail.com>
2022-10-18 16:38:16 -0700

    print arguments file

--- print_args.c
+++ print_args.c
@@ -20,7 +20,7 @@ int print_args(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', pr_char}, {'s', pr_string}, {'%', pr_percent},
+		{'c', pr_char}, {'s', pr_str}, {'%', pr_percent},
 		{'i', pr_int}, {'d', pr_int}, {'b', pr_binary},
 		{'u', pr_unsigned}, {'o', pr_octal}, {'x', pr_hexadecimal},
 		{'X', pr_hexa_upper}, {'p', pr_pointer}, {'S', pr_non_printable},
@@ -50,16 +50,4 @@ int print_args(const char *fmt, int *ind, va_list list, char buffer[],
 		return (unknow_len);
 	}
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
 }
