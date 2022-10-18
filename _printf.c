5f97601a770ef6c8a27505a125e6b41dd08ee388
cooldebs <doduronbi83@gmail.com>
2022-10-18 13:00:10 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -36,7 +36,7 @@ int _printf(const char *format, ...)
 				;
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list,
+			printed = print_args(format, &i, list,
 					buffer, flags, width,
 					precision, size);
 			if (printed == -1)
